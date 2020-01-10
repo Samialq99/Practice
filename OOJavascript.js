7c0c877f9d8c8d2fd8dc3c7d0c6ab178ffb22da3
Sammy <sarmad99@msn.com>
2020-01-10 00:49:57 -0500

    second

--- OOJavascript.js
+++ OOJavascript.js
@@ -4,7 +4,21 @@ let car = {
   carColor: function() {
     //noticefunction name : function ()
     console.log("Red");
-  }
+  } // No (,) after the very last key:value pair or attribute or function
 }; // must end creating an object like creating a variable
 
 car.carColor(); // when calling an object method you type Object.Method();
+
+const circle = {
+  // key:value pairs
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1
+  },
+  draw: function() {
+    console.log("Draw");
+  }
+};
+
+circle.draw();
