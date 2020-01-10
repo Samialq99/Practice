1c110132ca2f58b0ae0bb64ca9ef861fe6043f87
Sammy <sarmad99@msn.com>
2020-01-10 00:38:59 -0500

    First upload

---
+++ OOJavascript.js
@@ -0,0 +1,10 @@
+let car = {
+  model: "Mitsubishi", // separate all attribs by a (,)
+  make: "Lancer",
+  carColor: function() {
+    //noticefunction name : function ()
+    console.log("Red");
+  }
+}; // must end creating an object like creating a variable
+
+car.carColor(); // when calling an object method you type Object.Method();
